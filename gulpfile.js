a85a4803425a3ad1e39da669df090a8cb2165da8
Roy A Vanegas <roy@thecodeeducators.com>
2021-04-08 15:15:28 -0400

    Add the compress HTML task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
-const { src } = require(`gulp`);
+const { src, dest } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
 
 let validateHTML = () => {
     return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
 };
 
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
