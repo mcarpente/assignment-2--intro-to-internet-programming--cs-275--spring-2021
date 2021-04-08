c75a27a34698144f4d0cd3be9270e739ab73b996
Roy A Vanegas <roy@thecodeeducators.com>
2021-04-08 15:24:25 -0400

    Add task that runs two existing tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest } = require(`gulp`);
+const { src, dest, series } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 
@@ -14,3 +14,4 @@ let compressHTML = () => {
 
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
+exports.HTMLProcessing = series(validateHTML, compressHTML);
